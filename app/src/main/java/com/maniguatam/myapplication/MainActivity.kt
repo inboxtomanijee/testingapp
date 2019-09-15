21c8fe3e355afbd59e0ee60c6c90e2647d3dd176
manijee <inboxtomanijee@gmail.com>
2019-09-15 18:00:18 +0530

    This is testing file

---
+++ app/src/main/java/com/maniguatam/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.maniguatam.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this,"this is first file",Toast.LENGTH_SHORT).show()
+    }
+}
