ec604fe57fbba27e28553d5282a9bb12460b962c
manijee <inboxtomanijee@gmail.com>
2019-09-15 19:37:04 +0530

    this is second branch commit

--- app/src/main/java/com/maniguatam/myapplication/Main2Activity.kt
+++ app/src/main/java/com/maniguatam/myapplication/Main2Activity.kt
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this,"this is first file",Toast.LENGTH_SHORT).show()
-
+        setContentView(R.layout.activity_main2)
+        Toast.makeText(this,"adding coded from new branch", Toast.LENGTH_SHORT).show()
     }
 }
